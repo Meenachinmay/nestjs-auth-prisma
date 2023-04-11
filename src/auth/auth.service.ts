c4c06b2f4cb432ea2de1e215cf05b6579677d9b7
meenachinmay <Chinmayanand896@gmail.com>
2023-04-12 02:05:53 +0530

    able to genereate accessToken and refreshToken in nestjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,76 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bc from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   hashPassword(data: string) {
     return bc.hash(data, 10);
   }
 
+  async getToken(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+
   async signupLocal(dto: AuthDto): Promise<Tokens> {
     const hash = await this.hashPassword(dto.password);
-    const newUser = this.prismaService.user.create({
+    const newUser = await this.prismaService.user.create({
       data: {
         email: dto.email,
         hash,
         name: dto.name,
       },
     });
+
+    const tokens = await this.getToken(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
   }
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashPassword(rt);
+    await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
+
   signinLocal() {}
   logoutLocal() {}
   refreshTokens() {}
